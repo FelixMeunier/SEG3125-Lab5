09884f909a4e8c858f034af3e1a6786ca7f52ac9
Félix Meunier <fefelix2001@gmail.com>
2020-06-24 16:10:40 -0400

    final commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+//---Code from JQueryUI---
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+ //---End of code from JQueryUI---
